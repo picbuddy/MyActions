0e654907c32edee7e7f2ffa0112b17ef46ed10f9
picbuddy <picbuddy@naver.com>
2025-09-18 11:15:53 +0900

    modify code

--- src/metrics.py
+++ src/metrics.py
@@ -7,9 +7,9 @@ def mean(values):
 # 아래 함수들은 수업 중 "추가"해 보세요.
 # 추가 즉시 Pages 테이블에 새 행이 반영됩니다.
 
-# def median(values):
-#     """중앙값"""
-#     return _median(values)
+def median(values):
+    """중앙값"""
+    return _median(values)
 
 # def min_value(values):
 #     """최솟값"""
